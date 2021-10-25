96240d2ca3f4ec7f73f499b1521c6fe150194759
kandksoftware <57774027+kandksoftware@users.noreply.github.com>
2021-10-25 20:05:47 +0400

    changed

--- js/build.js
+++ js/build.js
@@ -133,7 +133,7 @@ const main = () => {
     img:'resources/app-store-badge.svg'
   },{
     name:'Google Play',
-    link:'https://play.google.com/store/apps/details?id=com.kandksoftware.macroterminalpro',
+    link:'https://kandksoftware.github.io/saas/',
     img:'resources/google-play-badge.svg'
   }])//modal
   
