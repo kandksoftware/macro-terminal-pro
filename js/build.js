3c2ac44f5d07eeab69b904e3e0b8f8d4dba58127
kandksoftware <57774027+kandksoftware@users.noreply.github.com>
2024-06-05 19:13:57 +0400

    request demo added

--- js/build.js
+++ js/build.js
@@ -270,4 +270,16 @@ const main = () => {
       })
     })
   }
+
+  const iframeSpinnerLoading = () => {
+    const ife = document.querySelectorAll('.iframe-container')
+    for (let i = 0, l = ife.length; i < l; i++) {
+      const spinner = ife[i].querySelector('.iframe-spinner')
+      const iframe = ife[i].querySelector('iframe')
+      spinner.style.display = 'block'
+      iframe.addEventListener('load', () => spinner.style.display = 'none')
+    }
+  }
+
+  iframeSpinnerLoading()
 }
